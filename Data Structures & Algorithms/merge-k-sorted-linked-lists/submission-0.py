ee6c26b6a4be92421738d55d245425d81c3a54c6
DeviVarshitha <devivarshithadosapati123@gmail.com>
2026-08-24 12:05:05 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        ListNode.__lt__ = lambda self, other: self.val < other.val
+        
+        heap = []
+        
+        for head in lists:
+            if head:
+                heapq.heappush(heap, head)
+        
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while heap:
+           
+            smallest_node = heapq.heappop(heap)
+            
+            curr.next = smallest_node
+            curr = curr.next
+           
+            if smallest_node.next:
+                heapq.heappush(heap, smallest_node.next)
+                
+        return dummy.next
